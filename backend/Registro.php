170f19e729bdbd6d3085f8290cf0a01ee071f8ef
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-11-07 15:10:12 -0500

    Update Registro.php

--- backend/Registro.php
+++ backend/Registro.php
@@ -103,6 +103,6 @@ if(isset($_POST)){
                  $_SESSION["error"]= $error ;
 
             }
-      header("location:/includes/registro.php");
+      header("location:/app/includes/registro.php");
       }
 ?>
