6295602216f3f026f12c51c949c6ab9eae92cea9
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-11-07 14:00:02 -0500

    Update conexionDB.php

--- funciones/conexionDB.php
+++ funciones/conexionDB.php
@@ -1,5 +1,5 @@
 <?php 
  /*"se crea la conexion ala base de datos"*/
-$db=mysqli_connect("containers-us-west-54.railway.app","root","sp4Mowi6BZgG3LBIu0J0","sp4Mowi6BZgG3LBIu0J0");
+$db=mysqli_connect("containers-us-west-54.railway.app","root","sp4Mowi6BZgG3LBIu0J0","sp4Mowi6BZgG3LBIu0J0","6874");
 session_start();
 ?>
