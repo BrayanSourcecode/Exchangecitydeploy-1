66d39eec36af5491f1c505e5d27a452fcfba0ecf
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-11-07 15:37:48 -0500

    Update Registro.php

--- backend/Registro.php
+++ backend/Registro.php
@@ -103,6 +103,6 @@ if(isset($_POST)){
                  $_SESSION["error"]= $error ;
 
             }
-header("location: /includes/registro.php");
+header("location: https://exchangecity2-production.up.railway.app/includes/registro.php");
  }
 ?>
