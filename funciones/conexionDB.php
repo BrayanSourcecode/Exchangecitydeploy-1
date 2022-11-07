f82f54db7a66de99b926b3e6febb93fc2a656118
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-11-07 14:06:16 -0500

    Update conexionDB.php

--- funciones/conexionDB.php
+++ funciones/conexionDB.php
@@ -1,5 +1,10 @@
 <?php 
  /*"se crea la conexion ala base de datos"*/
-$db=mysqli_connect("containers-us-west-54.railway.app","root","sp4Mowi6BZgG3LBIu0J0","sp4Mowi6BZgG3LBIu0J0","6874");
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
+$db=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
 session_start();
 ?>
