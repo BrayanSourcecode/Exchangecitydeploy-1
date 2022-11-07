d9793fd79d6a5835664cba34005d06d4dcdc0220
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-11-07 13:53:30 -0500

    Update conexionDB.php

--- funciones/conexionDB.php
+++ funciones/conexionDB.php
@@ -1,5 +1,5 @@
 <?php 
  /*"se crea la conexion ala base de datos"*/
-$db=mysqli_connect("$_ENV['DB_HOST']","$_ENV['DB_USER']","$_ENV['DB_PASSWORD']","$_ENV['DB_NAME']");
+$db=mysqli_connect($_ENV['DB_HOST'],$_ENV['DB_USER'],$_ENV['DB_PASSWORD'],$_ENV['DB_NAME']);
 session_start();
 ?>
