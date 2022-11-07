e3888410d36be68612e17925b54b4f548333f767
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-11-07 14:11:36 -0500

    Update acount.php

--- Includes/acount.php
+++ Includes/acount.php
@@ -1,10 +1,10 @@
 <?php 
-include_once "/wamp64/www/Exchangecity/proyectoSena/funciones/masFunciones.php";
+include_once "/app/funciones/masFunciones.php";
 if(isset($_SESSION["usuario"])):?>
 <div class="acount">
-      <a href="/exchangecity/proyectoSena/Includes/datosusuario.php">Datos Personales</a>
-      <a href="/exchangecity/proyectoSena/Includes/publicarProducto.php">Crear Publicacion</a>
-      <a href="/exchangecity/proyectoSena/Includes/misPublicaciones.php">Mis publicaciones</a>
+      <a href="/Includes/datosusuario.php">Datos Personales</a>
+      <a href="/Includes/publicarProducto.php">Crear Publicacion</a>
+      <a href="/Includes/misPublicaciones.php">Mis publicaciones</a>
       <?php $pubUsu= ConsultarPublicacionesUsuario($db,$_SESSION["login_correcto"]["usu_codigo"]);
         $usuPub=mysqli_fetch_assoc($pubUsu);
         $intercambios_activos=IntercambioUsuarioProveedor($db,$_SESSION["login_correcto"]["usu_codigo"]);
@@ -14,29 +14,29 @@ if(isset($_SESSION["usuario"])):?>
 
       <?php if(isset($usuPub) &&  $tres==false || $cuatro==1):?>
     
-        <a href="/exchangecity/proyectoSena/Includes/intercambio/crearIntercambio.php">Intercambio</a>
+        <a href="/Includes/intercambio/crearIntercambio.php">Intercambio</a>
         
       <?php endif;?>
       
       <?php if($tres==false || $cuatro==false):?>
-          <a href="/exchangecity/proyectoSena/Includes/intercambio/2Intercambio.php">Opciones Intercambio</a>
+          <a href="/Includes/intercambio/2Intercambio.php">Opciones Intercambio</a>
         <?php endif;?>
     </div>
-<script src="/exchangecity/PROYECTOSENA/js/acount.js"></script>
+<script src="/js/acount.js"></script>
 <?php endif;?>
 
 
 <?php 
 if(isset($_SESSION["usuario_administrador"])):?>
 <div class="acount">
-      <a href="/exchangecity/proyectoSena/Includes/administrador/crudUsuarios">Datos Usuarios</a>
-      <a href="/exchangecity/proyectoSena/Includes/administrador/crudPublicacion.php">Datos Publicaciones</a>
-      <a href="/exchangecity/proyectoSena/Includes/administrador/crudVerificado.php">Verificicacion</a>
-      <a href="/exchangecity/proyectoSena/Includes/administrador/crudAdministrador.php">Administradores</a>
+      <a href="/Includes/administrador/crudUsuarios">Datos Usuarios</a>
+      <a href="/Includes/administrador/crudPublicacion.php">Datos Publicaciones</a>
+      <a href="/Includes/administrador/crudVerificado.php">Verificicacion</a>
+      <a href="/Includes/administrador/crudAdministrador.php">Administradores</a>
     </div>
 
 
 
 
-<script src="/exchangecity/PROYECTOSENA/js/acount.js"></script>
+<script src="/js/acount.js"></script>
 <?php endif;?>
