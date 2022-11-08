e94b46a316420930dd9e4b61d7500bc400068a26
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-11-08 06:50:26 -0500

    Update eliminarPublicacion.php

--- backend/eliminarPublicacion.php
+++ backend/eliminarPublicacion.php
@@ -1,12 +1,12 @@
 <?php 
-include_once "/wamp64/www/Exchangecity/proyectoSena/funciones/conexionDB.php";
+include_once "/app/funciones/conexionDB.php";
 
 $pub_codigo=$_REQUEST["pub_codigo"] ;
 $sql2="DELETE FROM publicacion WHERE pub_codigo='$pub_codigo'";
 if(mysqli_query($db,$sql2)){
     
     $_SESSION["publicacion_eliminada"]="la publicacion se elimino correctamente";
-    header("location: /exchangecity/proyectosena/includes/misPublicaciones.php");
+    header("location: /Includes/misPublicaciones.php");
 }else{
         echo "error".mysqli_error($db) ;
 }
