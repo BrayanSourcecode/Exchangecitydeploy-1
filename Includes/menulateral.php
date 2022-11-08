c2ebf296a35a55c843c5363a0fb113893d4f6afc
AlejandroSantanilla <111874768+AlejandroSantanilla@users.noreply.github.com>
2022-11-08 07:03:31 -0500

    Update menulateral.php

--- Includes/menulateral.php
+++ Includes/menulateral.php
@@ -1,39 +1,39 @@
 <!-- Menu lateral -->
 <nav id="menu">
     <div class="icono">
-      <a class="menuObjeto" href="/exchangecity/PROYECTOSENA/index.php">
-        <img class="menuIcono" src="/exchangecity/PROYECTOSENA/imagenes/Navegacion/svg/house.svg" alt="inicio.svg">
+      <a class="menuObjeto" href="/index.php">
+        <img class="menuIcono" src="/imagenes/Navegacion/svg/house.svg" alt="inicio.svg">
         <span>Inicio</span>
       </a>
     </div>
     <div class="icono">
-      <a class="menuObjeto" href="/exchangecity/proyectoSena/Includes/categorias.php?tipo=TV">
-        <img class="menuIcono" src="/exchangecity/PROYECTOSENA/imagenes/Navegacion/svg/tv.svg" alt="televisores.svg">
+      <a class="menuObjeto" href="/Includes/categorias.php?tipo=TV">
+        <img class="menuIcono" src="/imagenes/Navegacion/svg/tv.svg" alt="televisores.svg">
         <span>TV</span>
       </a>
     </div>
     <div class="icono">
-      <a class="menuObjeto" href="/exchangecity/proyectoSena/Includes/categorias.php?tipo=Movil">
-        <img class="menuIcono" src="/exchangecity//PROYECTOSENA/imagenes/Navegacion/svg/smartphone.svg" alt="movil.svg">
+      <a class="menuObjeto" href="/Includes/categorias.php?tipo=Movil">
+        <img class="menuIcono" src="/imagenes/Navegacion/svg/smartphone.svg" alt="movil.svg">
         <span>Movil</span>
       </a>
     </div>
     <div class="icono">
-      <a class="menuObjeto" href="/exchangecity/proyectoSena/Includes/categorias.php?tipo=Pc">
-        <img class="menuIcono" src="/exchangecity//PROYECTOSENA/imagenes/Navegacion/svg/pc.svg" alt="computadoras.svg">
+      <a class="menuObjeto" href="/Includes/categorias.php?tipo=Pc">
+        <img class="menuIcono" src="/imagenes/Navegacion/svg/pc.svg" alt="computadoras.svg">
         <span>PC</span>
       </a>
     </div>
     <div class="icono">
-      <a class="menuObjeto" href="/exchangecity/proyectoSena/Includes/categorias.php?tipo=Consolas">
-        <img class="menuIcono" src="/exchangecity//PROYECTOSENA/imagenes/Navegacion/svg/console-controller.svg" alt="consolas.svg">
+      <a class="menuObjeto" href="/Includes/categorias.php?tipo=Consolas">
+        <img class="menuIcono" src="/imagenes/Navegacion/svg/console-controller.svg" alt="consolas.svg">
         <span> Consolas</span>
       </a>
     
     </div>   
     <?php if(isset($_SESSION["login_correcto"])):  ?>
     <div class="acount_button icono">
-      <button class="bottom menuObjeto"><img class="menuIcono" src="/Exchangecity\proyectoSena\imagenes\navegacion\people_outline_white_24dp.svg" alt="">
+      <button class="bottom menuObjeto"><img class="menuIcono" src="\imagenes\navegacion\people_outline_white_24dp.svg" alt="">
       </button>
     </div>
      <?php endif?>
