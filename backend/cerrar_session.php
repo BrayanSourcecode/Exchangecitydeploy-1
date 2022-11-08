b665e35a8f917ded9bf2c64e99ffa586fbd44f0f
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-11-08 06:48:08 -0500

    Update cerrar_session.php

--- backend/cerrar_session.php
+++ backend/cerrar_session.php
@@ -3,5 +3,5 @@ session_start();
     if(isset($_SESSION["login_correcto"])){
     $_SESSION["login_correcto"]=session_destroy();
     }
-    header("location: ../index.php");
+    header("location: /index.php");
 ?>
