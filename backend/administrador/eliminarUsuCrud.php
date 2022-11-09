3c7e5c36d87b5a2d8d5dfced3a9a6dfff60161df
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-11-09 09:59:13 -0500

    Update eliminarUsuCrud.php

--- backend/administrador/eliminarUsuCrud.php
+++ backend/administrador/eliminarUsuCrud.php
@@ -1,6 +1,6 @@
 <?php 
 
-include_once "/wamp64/www/Exchangecity/proyectoSena/funciones/conexionDB.php";
+include_once "/app/funciones/conexionDB.php";
 
 $usuario=$_REQUEST["usu_codigo"] ;
 $sql2="DELETE FROM publicacion WHERE FK_dat_codigo_pd_usu ='$usuario'";
@@ -20,7 +20,7 @@ $sql1="DELETE FROM usuario WHERE usu_codigo='$usuario'";
 
 if(mysqli_query($db,$sql1)){
    
-    header("location: /exchangecity/proyectosena/includes/administrador/crudUsuarios.php");
+    header("location: /Includes/administrador/crudUsuarios.php");
     $_SESSION["usuario_crud"]="Se elimino todos los datos referentes a el usuario";
     
 }else{
