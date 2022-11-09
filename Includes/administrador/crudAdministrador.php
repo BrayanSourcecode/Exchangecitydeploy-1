2e089cd96f1567b23cc139d3848c9d142b7bad1f
AlejandroSantanilla <111874768+AlejandroSantanilla@users.noreply.github.com>
2022-11-09 09:59:33 -0500

    Update crudAdministrador.php

--- Includes/administrador/crudAdministrador.php
+++ Includes/administrador/crudAdministrador.php
@@ -1,8 +1,8 @@
 <?php
-include_once "/wamp64/www/Exchangecity/proyectoSena/funciones/conexionDB.php";
-include_once "/wamp64/www/exchangecity/proyectoSena/Includes/header.php";
-include_once "/wamp64/www/exchangecity/proyectoSena/Includes/menulateral.php";
-include_once "/wamp64/www/exchangecity/proyectoSena/funciones/masFunciones.php";
+include_once "/app/funciones/conexionDB.php";
+include_once "/app/Includes/header.php";
+include_once "/app/Includes/menulateral.php";
+include_once "/app/funciones/masFunciones.php";
 ?>
 <?php 
 $sql="SELECT *FROM usuario WHERE FK_rol_codigo_ur='1'";
@@ -44,7 +44,7 @@ $consulta=mysqli_query($db,$sql);
                 <th class="datoCrudUsu"><?php echo $row["usu_documento"] ?></th>
                 <th class="datoCrudUsu"><?php echo $row["FK_rol_codigo_ur"] ?></th>
                 <th class="datoCrudUsu"><?php echo $row["FK_ciu_codigo_uc"] ?></th>
-                <th class="datoCrudUsu"><a class="crudBoton" href="/exchangecity/proyectoSena/backen/administrador/administradorCrud.php?usu_codigo=<?php echo $row["usu_codigo"]?>">Eliminar</a></th>
+                <th class="datoCrudUsu"><a class="crudBoton" href="/backen/administrador/administradorCrud.php?usu_codigo=<?php echo $row["usu_codigo"]?>">Eliminar</a></th>
                 
             </tr>
             <?php endwhile;?>
@@ -53,11 +53,11 @@ $consulta=mysqli_query($db,$sql);
         </div>
         
     
-    <?php include_once "/wamp64/www/Exchangecity/proyectoSena/Includes/acount.php" ?>
+    <?php include_once "/app/Includes/acount.php" ?>
     </section>
   </section>
   <?php
-  include_once "/wamp64/www/exchangecity/proyectoSena/includes/footer.php";
+  include_once "/app/Includes/footer.php";
   ?>
   <!--limpiamos los flotados del aside -->
   <div style="clear: both"></div>
