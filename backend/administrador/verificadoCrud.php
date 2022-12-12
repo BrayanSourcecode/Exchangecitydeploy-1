713a1a91e2deaa0aa0f3a382e9e7154c3b41ef4c
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-12-12 10:51:14 -0500

    Update verificadoCrud.php

--- backend/administrador/verificadoCrud.php
+++ backend/administrador/verificadoCrud.php
@@ -1,10 +1,11 @@
 <?php 
 include_once "/app/funciones/conexionDB.php";
 
-$usuario=$_REQUEST["usu_codigo"] ;
+$usuario=$_REQUEST["usu_codigo"];
 $sql2="DELETE FROM publicacion WHERE FK_dat_codigo_pd_usu ='$usuario'";
 
 if(mysqli_query($db,$sql2)){ 
+        
 }else{
         echo "error".mysqli_error($db) ;
 }
@@ -14,7 +15,7 @@ if(mysqli_query($db,$sql)){
     header("location: /Includes/administrador/crudVerificado.php");
     $_SESSION["eliminar_ver"]="El verificado del usuario se elimino correctamente";
 }else{
-        echo "error".mysqli_error($db) ;
+        echo "error".mysqli_error($db);
         
 }
 
